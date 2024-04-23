b0ca32475a5cba8147d2d54b8b4e5c0bf7f5e806
Ange Michel <angemicheluwizeye@gmail.com>
2024-04-23 12:00:34 +0100

    feat(email verification): implement email verification

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,7 +3,9 @@ import { UserModelAttributes } from "../database/models/User";
 import { generateAccessToken } from "../helpers/security.helpers";
 import { HttpException } from "../utils/http.exception";
 import passport, { CustomVerifyOptions } from "../middlewares/passport";
-
+import { Token } from "../database/models/token";
+import sendEmail from "../utils/email";
+sendEmail;
 
 interface InfoAttribute extends CustomVerifyOptions {}
 
@@ -12,32 +14,39 @@ const registerUser = async (
 	res: Response,
 	next: NextFunction,
 ) => {
-  try {
-    if (req.body) {
-      passport.authenticate(
-        "signup",
-        (err: Error, user: UserModelAttributes, info: InfoAttribute) => {
-          if (!user) {
-            return res
-              .status(info.statusNumber || 400)
-              .json(new HttpException(info.status, info.message));
-          }
-          req.login(user, async () => {
-            const token = generateAccessToken({ id: user.id, role: user.role });
-            const response = new HttpException(
-              "SUCCESS",
-              "Account Created successfully!"
-            ).response();
-            res.status(201).json({ ...response, token });
-          });
-        }
-      )(req, res, next);
-    }
-  } catch (error) {
-    res
-      .status(500)
-      .json(new HttpException("SERVER FAILS", "Something went wrong!"));
-  }
+	try {
+		if (req.body) {
+			passport.authenticate(
+				"signup",
+				(err: Error, user: UserModelAttributes, info: InfoAttribute) => {
+					if (!user) {
+						return res
+							.status(info.statusNumber || 400)
+							.json(new HttpException(info.status, info.message));
+					}
+					req.login(user, async () => {
+						const token = generateAccessToken({ id: user.id, role: user.role });
+						await Token.create({ token });
+						const message = `${process.env.BASE_URL}/users/account/verify/${token}`;
+						await sendEmail({
+							email: user.email,
+							subject: "Verify Email",
+							message: message,
+						});
+						const response = new HttpException(
+							"SUCCESS",
+							"Account Created successfully!",
+						).response();
+						res.status(201).json({ ...response, token });
+					});
+				},
+			)(req, res, next);
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json(new HttpException("SERVER FAILS", "Something went wrong!"));
+	}
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
@@ -50,10 +59,10 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 					.json(new HttpException("BAD REQUEST", "Bad Request!"));
 			}
 
-      if (info)
-        return res
-          .status(404)
-          .json(new HttpException("NOT FOUND", info.message));
+			if (info)
+				return res
+					.status(404)
+					.json(new HttpException("NOT FOUND", info.message));
 
 			(req as any).login(user, (err: Error) => {
 				if (err) {
@@ -64,18 +73,18 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
 				const { id, role } = user;
 
-        const token = generateAccessToken({ id, role });
-        const response = new HttpException(
-          "SUCCESS",
-          "Logged in to your account successfully!"
-        ).response();
-        return res.status(200).json({ ...response, token });
-      });
-    }
-  )(req, res, next);
+				const token = generateAccessToken({ id, role });
+				const response = new HttpException(
+					"SUCCESS",
+					"Logged in to your account successfully!",
+				).response();
+				return res.status(200).json({ ...response, token });
+			});
+		},
+	)(req, res, next);
 };
 
 export default {
 	registerUser,
-	login
+	login,
 };
--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -5,89 +5,90 @@ import { User } from "../database/models/User";
 import { isValidPassword } from "../utils/password.checks";
 import { hashPassword } from "../utils/password";
 export interface CustomVerifyOptions {
-  message: string;
-  status: string;
-  statusNumber?: number;
+	message: string;
+	status: string;
+	statusNumber?: number;
 }
 passport.serializeUser(function (user: any, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function (user: any, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.use(
-  "signup",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async (req, email, password, done) => {
-      try {
-        const data = {
-          email: email.trim(),
-          password: await hashPassword(password),
-          confirmPassword: await hashPassword(req.body.confirmPassword),
-          userName:
-            req.body.userName == null
-              ? req.body.email.split("@")[0]
-              : req.body.userName,
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          role: "BUYER",
-        };
-        const userEXist = await User.findOne({
-          where: {
-            email: data.email,
-          },
-        });
-        if (userEXist) {
-          const options: CustomVerifyOptions = {
-            statusNumber: 409,
-            message: "User already exist!",
-            status: "CONFLICT",
-          };
-          return done(null, false, options);
-        }
-        const user = await User.create({ ...data });
-        done(null, user);
-      } catch (error) {
-        done(error);
-      }
-    }
-  )
+	"signup",
+	new LocalStrategy(
+		{
+			usernameField: "email",
+			passwordField: "password",
+			passReqToCallback: true,
+		},
+		async (req, email, password, done) => {
+			try {
+				const data = {
+					email: email.trim(),
+					password: await hashPassword(password),
+					confirmPassword: await hashPassword(req.body.confirmPassword),
+					userName:
+						req.body.userName == null
+							? req.body.email.split("@")[0]
+							: req.body.userName,
+					firstName: req.body.firstName,
+					lastName: req.body.lastName,
+					role: "BUYER",
+					isVerified: false,
+				};
+				const userEXist = await User.findOne({
+					where: {
+						email: data.email,
+					},
+				});
+				if (userEXist) {
+					const options: CustomVerifyOptions = {
+						statusNumber: 409,
+						message: "User already exist!",
+						status: "CONFLICT",
+					};
+					return done(null, false, options);
+				}
+				const user = await User.create({ ...data });
+				done(null, user);
+			} catch (error) {
+				done(error);
+			}
+		},
+	),
 );
 
 passport.use(
-  "login",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async (_req: Request, email, password, done) => {
-      try {
-        const user = await User.findOne({ where: { email } });
+	"login",
+	new LocalStrategy(
+		{
+			usernameField: "email",
+			passwordField: "password",
+			passReqToCallback: true,
+		},
+		async (_req: Request, email, password, done) => {
+			try {
+				const user = await User.findOne({ where: { email } });
 
-        if (!user) return done(null, false, { message: "Wrong credentials!" });
+				if (!user) return done(null, false, { message: "Wrong credentials!" });
 
-        const currPassword = user.dataValues.password;
+				const currPassword = user.dataValues.password;
 
-        const isValidPass = await isValidPassword(password, currPassword);
+				const isValidPass = await isValidPassword(password, currPassword);
 
-        if (!isValidPass)
-          return done(null, false, { message: "Wrong credentials!" });
+				if (!isValidPass)
+					return done(null, false, { message: "Wrong credentials!" });
 
-        return done(null, user);
-      } catch (error) {
-        done(error);
-      }
-    }
-  )
+				return done(null, user);
+			} catch (error) {
+				done(error);
+			}
+		},
+	),
 );
 
 export default passport;
